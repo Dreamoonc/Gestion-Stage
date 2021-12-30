5d7a9aa2a60cbf9f0955504d874b6fb620e3ff36
Dreamoonc <ahlemlabri@gmail.com>
2021-12-30 01:23:57 +0100

    done

--- gestionstage/AppGestion/models.py
+++ gestionstage/AppGestion/models.py
@@ -1,51 +1,78 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime
+
+NIV_ETUDE_STAGE=(
+(1,"CP1"),
+(3,"CS1"),
+(5,"CS3")
+)
+NIV_ETUDE=(
+(1,"CP1"),
+(2,"CP2"),
+(3,"CS1"),
+(4,"CS2"),
+(5,"CS3")
+)
+
 
 
 class Stagiaire (models.Model):
     matricule= models.CharField(max_length=15,primary_key=True)
     NomStagiaire=models.CharField(max_length=15)
     PrenomStagiaire=models.CharField(max_length=30)
-    NivEtude=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],)
+    NivEtude=models.IntegerField(choices=NIV_ETUDE)
+    def __str__(self):
+        return self.NomStagiaire +' '+self.PrenomStagiaire
+    class Meta:
+        ordering = ['NivEtude']
 
 
 class Encadrant (models.Model):
     NomPrenomEn=models.CharField(max_length=40)
+    def __str__(self):
+        return self.NomPrenomEn 
+    class Meta:
+        ordering = ['NomPrenomEn']
 
 
-class OrganismedAccueil (models.Model):
+class Organisme (models.Model):
     NomOrganisme= models.CharField(max_length=40,primary_key=True)
     typeOr=models.CharField(max_length=20,null=True)
+    def __str__(self):
+        return self.NomOrganisme 
+
 
 
 class Promoteur (models.Model):
     NomPrenomPro=models.CharField(max_length=40)
-    Organisme = models.ForeignKey("OrganismedAccueil",on_delete=models.CASCADE)
+    Organisme = models.ForeignKey("Organisme",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.NomPrenomPro
+    class Meta:
+        ordering = ['NomPrenomPro']
 
 
-class TypeStage (models.Model):
-    NomType= models.CharField(max_length=3,primary_key=True)
-    NivEtude=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],)
-    periode=models.CharField(max_length=30)
+
 
 
 class Stage (models.Model):
     NomStage = models.CharField(max_length=40)
-    Sujet = models.CharField(max_length=60,unique=True)
-    TypeStage= models.ForeignKey("TypeStage",on_delete=models.CASCADE)
-    Organisme = models.ForeignKey("OrganismedAccueil",on_delete=models.CASCADE)
+    NivEtude= models.IntegerField(choices=NIV_ETUDE_STAGE)
+    Organisme = models.ForeignKey("Organisme",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.NomStage
 
 
-class Faire_Stage (models.Model):
-    Organisme=models.ForeignKey("OrganismedAccueil",on_delete=models.CASCADE)
+class Fiche_Stage (models.Model):
+    Organisme=models.ForeignKey("Organisme",on_delete=models.CASCADE)
     Stage=models.ForeignKey("Stage",on_delete=models.CASCADE)
     matricule=models.ForeignKey("Stagiaire",on_delete=models.CASCADE)
+    NivEtude =models.IntegerField(choices=NIV_ETUDE_STAGE)
     Encadrant=models.ForeignKey("Encadrant",on_delete=models.CASCADE)
     Promoteur=models.ForeignKey("Promoteur",on_delete=models.CASCADE)
-    NivEtude=models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],)
     AnneeCourante=models.IntegerField(default=datetime.datetime.now().year)
     Groupe=models.IntegerField()
+    Sujet = models.CharField(max_length=60,unique=True,null=True,blank=True)
 
     class Meta:
         unique_together = (('matricule','NivEtude','AnneeCourante'),)
--- gestionstage/AppGestion/views.py
+++ gestionstage/AppGestion/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index (request):
+    stagiaires=Stagiaire.objects.all()
+    encadrants=Encadrant.objects.all()
+    organismes=Organisme.objects.all()
+    promoteurs=Promoteur.objects.all()
+    stages=Stage.objects.all()
+    fiche_Stages=Fiche_Stage.objects.all()
+    
+    return render(request,'index.html',{'stagiaire':stagiaires,'encadrant':encadrants
+                   ,'organisme':organismes,'promoteur':promoteurs,'stage':stages,'ficheS':fiche_Stages})
