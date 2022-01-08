daf5e0c31e656c51da3852a61dd1216428c864c6
mdjassia <mdjassia@gmail.com>
2022-01-08 01:41:09 +0000

    suppression des ligne

--- gestionstage/AppGestion/urls.py
+++ gestionstage/AppGestion/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('',views.index),
+    path('delete_stagaire/<str:myid>/',delete_Stagaire,name='delete_Stagaire'),
+    path('delete_encaderant/<int:myid>/',delete_encaderant,name='delete_encaderant'),
+    path('delete_organisme/<str:myid>/',delete_organisme,name='delete_organisme'),
+    path('delete_promoteur/<int:myid>/',delete_promoteur,name='delete_promoteur'),
+    path('delete_stage/<int:myid>/',delete_stage,name='delete_stage'),
+    
 ]
--- gestionstage/AppGestion/views.py
+++ gestionstage/AppGestion/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,HttpResponse
+from django.contrib import  messages  
+from django.http.response import HttpResponse
 from .models import *
+
 # Create your views here.
 def index (request):
     stagiaires=Stagiaire.objects.all()
@@ -11,3 +14,36 @@ def index (request):
     
     return render(request,'index.html',{'stagiaire':stagiaires,'encadrant':encadrants
                    ,'organisme':organismes,'promoteur':promoteurs,'stage':stages,'ficheS':fiche_Stages})
+
+def delete_Stagaire (request,myid) :
+    item = Stagiaire.objects.get(matricule =myid)
+    item.delete()
+    messages.info(request,'item delete seccely ')
+    return redirect(index)
+
+def delete_encaderant (request,myid) :
+    item = Encadrant.objects.get(id =myid)
+    item.delete()
+    messages.info(request,'item delete seccely ')
+    return redirect(index)
+
+def delete_organisme (request,myid) :
+    item = Organisme.objects.get(NomOrganisme =myid)
+    item.delete()
+    messages.info(request,'item delete seccely ')
+    return redirect(index)
+
+def delete_promoteur (request,myid) :
+    item = Promoteur.objects.get(id =myid)
+    item.delete()
+    messages.info(request,'item delete seccely ')
+    return redirect(index)
+
+def delete_stage (request,myid) :
+    item = Stage.objects.get(id =myid)
+    item.delete()
+    messages.info(request,'item delete seccely ')
+    return redirect(index)
+    
+
+
